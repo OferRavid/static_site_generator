c5518d021d0b0ff2d3b3f0716e0b5882f652bdad
OferRavid <ofer.rvd@gmail.com>
2025-03-13 04:23:42 +0200

    Use GitHub site generator

--- src/generate_content.py
+++ src/generate_content.py
@@ -9,7 +9,7 @@ def extract_title(markdown):
             return line[2:]
     raise ValueError("Invalid markdown: missing h1 header.")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     if not os.path.exists(from_path) or not os.path.exists(template_path):
         raise FileNotFoundError("Something's wrong with the paths provided...")
     print(f"Generating page from {from_path} to {dest_path} using template: {template_path}")
@@ -19,11 +19,15 @@ def generate_page(from_path, template_path, dest_path):
         template = template_file.read()
     content = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    page = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    page = template.replace(
+        "{{ Title }}", title).replace(
+            "{{ Content }}", content).replace(
+                "href=\"/", f"href=\"{basepath}").replace(
+                    "src=\"/", f"src=\"{basepath}")
     with open(dest_path, 'w') as html:
         html.write(page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dir_path_content) or not os.path.exists(template_path) or not os.path.exists(dest_dir_path):
         raise FileNotFoundError("Something's wrong with the paths provided...")
     for dir_or_file in os.listdir(dir_path_content):
@@ -38,8 +42,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             except FileNotFoundError:
                 print("Something is wrong with the path provided. Check it and try again.")
                 return
-            generate_pages_recursive(source_dir_or_file, template_path, target_dir_or_file)
+            generate_pages_recursive(source_dir_or_file, template_path, target_dir_or_file, basepath)
         elif os.path.isfile(source_dir_or_file):
             dest_path = Path(target_dir_or_file).with_suffix(".html")
-            generate_page(source_dir_or_file, template_path, dest_path)
+            generate_page(source_dir_or_file, template_path, dest_path, basepath)
 
--- src/main.py
+++ src/main.py
@@ -1,23 +1,27 @@
+import sys
 from copy_static import copy_dir
 from generate_content import generate_pages_recursive
 
 import os, shutil
 
 dir_path_static = "static"
-dir_path_public = "public"
+dir_path_docs = "docs"
 dir_path_content = "content"
 template_path = "template.html"
 
 
 def main():
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    basepath = "/"
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
     if not os.path.exists(dir_path_static):
         print("Missing static directory... Aborting.")
         exit(1)
-    copy_dir(dir_path_static, dir_path_public)
+    copy_dir(dir_path_static, dir_path_docs)
     try:
-        generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+        generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
     except FileNotFoundError:
         print("Missing file or directory")
         exit(1)
